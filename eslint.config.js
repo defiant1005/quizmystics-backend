4f07be545a68ea13b4cbeef84655e5895742203b
ridiiatov <r.idiyatov@iilab.ru>
2025-04-01 11:43:00 +0300

    feat: initial

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import js from '@eslint/js';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import unusedImports from 'eslint-plugin-unused-imports';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.js'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        process: true,
+        __dirname: true,
+        module: true,
+        require: true,
+        console: true,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      'no-console': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+      'unused-imports/no-unused-imports': 'warn',
+    },
+  },
+];
